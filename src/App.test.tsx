042bc092cac3b5a743dc63e851f71870f5996a3c
goodyguts <adam.j.carruthers@hotmail.co.uk>
2021-10-16 16:41:35 +0100

    Fresh project configured correctly

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { store } from "./data/store";
+import App from "./App";
 
-test('renders learn react link', () => {
-  const { getByText } = render(
-    <Provider store={store}>
-      <App />
-    </Provider>
-  );
-
-  expect(getByText(/learn/i)).toBeInTheDocument();
+describe("the app", () => {
+  test("renders without crashing", () => {
+    render(
+      <Provider store={store}>
+        <App />
+      </Provider>
+    );
+  });
 });
