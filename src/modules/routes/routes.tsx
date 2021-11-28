ec0b2775f20d271bc8f558b91742d0ebc6574dfa
goodyguts <adam.j.carruthers@hotmail.co.uk>
2021-11-28 18:34:54 +0000

    Created blank create match entry page

--- src/modules/routes/routes.tsx
+++ src/modules/routes/routes.tsx
@@ -6,6 +6,7 @@ import RouteNeedLogin from "./routeNeedsLogin";
 import PageIndex from "../pageIndex/pageIndex";
 import PageLogin from "../pageLogin/pageLogin";
 import Page404 from "./page404";
+import PageCreateMatchEntry from "../pageCreateMatchEntry/pageCreateMatchEntry";
 
 const Routes = () => (
   <Switch>
@@ -20,6 +21,9 @@ const Routes = () => (
       <div className="pp-first-section">
         <h1>HOME TO BE BUILT</h1>
       </div>
+    </RouteNeedLogin>
+    <RouteNeedLogin path="/create-match-entry">
+      <PageCreateMatchEntry />
     </RouteNeedLogin>
 
     <Route path="*">
