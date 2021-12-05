4ae7421561efdf70946db5a2bee528a0d2d90e62
goodyguts <adam.j.carruthers@hotmail.co.uk>
2021-12-05 23:40:06 +0000

    Created base page with scrolling

--- src/modules/pageCreateMatchEntry/pageCreateMatchEntry.tsx
+++ src/modules/pageCreateMatchEntry/pageCreateMatchEntry.tsx
@@ -1,9 +1,65 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+import "./pageCreateMatchEntry.css";
 
 const PageCreateMatchEntry = () => {
+  const totalPages = 2;
+  const [currentPosition, setCurrentPosition] = useState<number>(0);
+
+  const scrollByNumberOfPages = (change: number) => {
+    setCurrentPosition(currentPosition + change);
+  };
+
   return (
     <div className="pp-first-section bg-blue-waves">
-      <h1>CREATE MATCH ENTRY TO BE BUILT</h1>
+      <div className="cme-outer">
+        <button
+          className="btn cme-arrow cme-arrow-left"
+          onClick={() => scrollByNumberOfPages(-1)}
+          disabled={currentPosition === 0}
+        >
+          <i className="fas fa-chevron-left" />
+        </button>
+        <button
+          className="btn cme-arrow cme-arrow-right"
+          onClick={() => scrollByNumberOfPages(1)}
+          disabled={currentPosition === totalPages - 1}
+        >
+          <i className="fas fa-chevron-right" />
+        </button>
+        <div
+          className="cme-pages-container"
+          style={{ marginLeft: `-${currentPosition * 100}vw` }}
+        >
+          <div className="cme-page">
+            <div className="flex-grow-1 p-3" />
+            <div className="cme-box">
+              <div className="cme-page-body">
+                <h5 className="text-secondary">Question 1/5</h5>
+                <div className="cme-fancy-h">
+                  <h1>Which album do you recommend?</h1>
+                </div>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="e.g: Lorde Melodrama"
+                />
+              </div>
+              <div className="cme-footer">
+                Create Matching Entry{" "}
+                <i className="text-nowrap">- Question 1/5</i>
+              </div>
+            </div>
+            <div className="flex-grow-1 p-3" />
+          </div>
+          <div className="cme-page">
+            <div className="flex-grow-1 p-3" />
+            <div className="cme-box">
+              <h1>Create matching entry</h1>
+            </div>
+            <div className="flex-grow-1 p-3" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
